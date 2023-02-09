7ef7d8aff3633d002df841cc34badc7199d6b213
Mukund <mukundkrishan@yahoo.com>
2023-02-09 12:38:48 +0530

    uploaded_files

---
+++ uploaded_files/938465proposal271/contact.php
@@ -0,0 +1,184 @@
+<!DOCTYPE HTML>
+<html dir="ltr" lang="en">
+
+
+<!-- Mirrored from wahabali.com/work/copious/contact.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 01 Oct 2022 06:53:50 GMT -->
+<head>
+    <?php
+    include_once('header.php');
+    ?>    
+</head>
+<body>
+    <!-- Loader Start -->
+    <div class="loader">
+        <div class="sk-rotating-plane"></div>
+    </div>
+    <!-- Loader End -->
+    <!-- Header Top Start -->
+    <?php
+    include_once('header_top.php');
+    ?>   
+    <?php
+    include_once('navigation.php');
+    ?>  
+        
+    <!-- Navbar End -->
+
+    <!-- Page Banner Start -->
+    <section id="page_banner" class="m-b-40">
+        <div class="container">
+            <div class="page-header text-center" style="margin-top: 50px;margin-bottom: 10px;">
+                <h2>Contact Us</h2>
+            </div>
+        </div>
+    </section>
+    <!-- Page Banner End -->
+
+    <!-- Contact Us Start -->
+    <!-- Map Start -->
+    <div id="contact_us_page">
+        <div class="container">
+            <div class="row p-b-20">
+                <div class="col-md-4">
+                    <h3>Registered Office</h3><br>
+                    <p>	A4/802 Tulip Violet<br>Sector 69, Gurugram<br>Harayana - 122101<br>India</p>
+                </div>
+                <div class="col-md-4">
+                <h3>Corporate Office</h3><br>
+                    <p>	S3 Tower, Cloud 9,<br>Sector 1, Vaishali Ghaziabada,<br> Uttar Pradesh - 201010<br>India</p>
+                </div>
+                <div class="col-md-4">
+                <h3>Branch Office</h3><br>
+                    <p>	Behind Reliance Mall<br>Near Ess Ell Motors The Office Dehradun,<br> Uttarakhand - 248001<br>India</p>
+                </div>
+            </div>
+            <!-- <div class="contact">
+                <div id="map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3501.4004272658044!2d77.34705645011101!3d28.64772668232545!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d22ea862c2475%3A0xe8ceefb930ca722!2sCONVOLUTION%20ENGINEERING%20CONSULTANCY%20LLP!5e0!3m2!1sen!2sin!4v1669983217601!5m2!1sen!2sin" width="1080" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div> -->
+        </div>
+    </div>
+    <!-- Map End -->
+    <section id="contact_form" class="p-t-60 p-b-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 p-b-10">
+                    <div class="heading">
+                        <div class="heading_border bg_red"></div>
+                        <p>Contact Us</p>
+                        <h2>WE WANT TO <span class="color_red">HEAR FROM YOU</span></h2>
+                    </div>
+                    <div class="row p-t-40">
+                        <form  id="contact-form" onSubmit="return false">
+
+                             <div class="col-md-12"><div id="result"></div></div>
+
+                             <div class="col-md-6 col-sm-6">
+                                <div class="single-query">
+                                    <input type="text" placeholder="Your Name*" class="keyword-input" required name="name" id="name">
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-sm-6">
+                                <div class="single-query">
+                                    <input type="email" placeholder="Email Address*" class="keyword-input" required name="email" id="email" >
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-sm-6">
+                                <div class="single-query">
+                                    <input type="text" placeholder="Phone Number*" required class="keyword-input"  name="phone" id="phone" >
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-sm-6">
+                                <div class="single-query">
+                                    <input type="text" placeholder="Subject" class="keyword-input" name="subject">
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="single-query">
+                                    <textarea placeholder="Message" name="message" id="message" required="required"></textarea>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <button type="submit" class="btn-light button-black" id="btn_submit" name="btn_submit">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-8 colxs-12">
+
+                    <div class="form_left p-b-80">
+                        <h3 class="text-uppercase">Get in Touch</h3>
+                        <div class="footer_1_contact">
+                        <p class="row">
+                        <div class="col-md-3">
+                            <i class="icon-telephone114"></i>
+                        </div>
+                        <div class="col-md-9">    
+                            +91-9910140812 <br>+91-9910179873
+                        </div>
+                        </p>
+                            <!-- <p><i class=" icon-icons142"></i>hello@copious.com</p>
+                            <br>
+                            <p><i class="icon-icons20"></i>Mon to Fri - 08:00 to 16:30</p>
+                            <br> -->
+                            <!-- <i class="icon-icons74"></i>
+                            <p>	G-704, Pearl Court, Ramprastha Greens, 
+                            <br>Vaishali Sec-7, Ghaziabad, Pin- 201010</p> -->
+                        </div>
+                    </div>
+
+                    
+
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!-- Contact Us End -->
+
+    <!-- Footer Start -->
+    <?php
+    include_once('footer.php');
+    ?>  
+
+    <!-- Footer End -->
+
+    <!--Required JS -->
+    <script src="js/jquery.2.2.3.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- SMOOTH SCROLL -->  
+    <script type="text/javascript" src="js/scroll-desktop.js"></script>
+    <script type="text/javascript" src="js/scroll-desktop-smooth.js"></script>
+
+    <script src="js/themepunch/jquery.themepunch.revolution.min.js"></script>
+    <script src="js/themepunch/jquery.themepunch.tools.min.js"></script>
+    <script src="js/themepunch/revolution.extension.layeranimation.min.js"></script>
+    <script src="js/themepunch/revolution.extension.navigation.min.js"></script>
+    <script src="js/themepunch/revolution.extension.parallax.min.js"></script>
+    <script src="js/themepunch/revolution.extension.slideanims.min.js"></script>
+    <script src="js/themepunch/revolution.extension.video.min.js"></script>
+    <script src="js/tab_slider.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/bootsnav.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/viedobox/video.js"></script>
+    <script src="js/jquery.parallax-1.1.3.js"></script>
+    <script src="js/bootstrap-number-input.js"></script>
+    <script src="js/renge_selectbox-0.2.min.js"></script>
+    <script src="js/range-Slider.min.js"></script>
+    <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyAOBKD6V47-g_3opmidcmFapb3kSNAR70U"></script>
+    <script src="js/gmaps.js"></script>
+    <script src="js/jquery.counterup.js"></script>
+    <script src="js/zelect.js"></script>
+    <!-- Progress  -->
+    <script src="js/progressbar.js"></script>
+    <script src="js/contact.js"></script>
+    <script src="js/copious_custom.js"></script>
+</body>
+
+
+<!-- Mirrored from wahabali.com/work/copious/contact.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 01 Oct 2022 06:53:51 GMT -->
+</html>
