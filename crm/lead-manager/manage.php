7ea4d30f0cb1449aaac1c6040d752b1762c70f12
Mukund <mukundkrishan@yahoo.com>
2023-02-27 15:53:31 +0530

    change in manage listing

--- crm/lead-manager/manage.php
+++ crm/lead-manager/manage.php
@@ -320,9 +320,9 @@
 		
 		$start = intval($_GET['start']);
 		$pagesize = intval($pagesize)==0?$pagesize=DEF_PAGE_SIZE:$pagesize;
-		$columns = "SELECT l.id, l.status, l.post_date, l.customer_name, l.proposal_address, l.assigned_to, l.form_type, l.post_by, l.city ";
+		$columns = "SELECT l.id, l.status, l.created_date, l.post_date, l.customer_name, l.proposal_address, l.assigned_to, l.form_type, l.post_by, l.city ";
 		$sql = " FROM #_leads l WHERE 1=1 AND l.is_deleted=0 AND l.lead_id=0 ".$conditionsQr." $adminRole ";
-		$order_by == '' ? $order_by = 'l.post_date' : true;
+		$order_by == '' ? $order_by = 'l.created_date' : true;
 		$order_by2 == '' ? $order_by2 = 'DESC' : true;
 		$sql_count = "SELECT count(*) ".$sql." GROUP BY l.id"; 
 		$sql .= " order by $order_by $order_by2 ";
@@ -337,9 +337,9 @@
 		//echo "test";
 		$start = intval($_GET['start']);
 		$pagesize = intval($pagesize)==0?$pagesize=DEF_PAGE_SIZE:$pagesize;
-		$columns = "SELECT l.id, l.status, l.post_date, l.customer_name, l.proposal_address, l.assigned_to, l.form_type, l.post_by, l.city,l.update_date ";
+		$columns = "SELECT l.id, l.status, l.created_date,l.post_date, l.customer_name, l.proposal_address, l.assigned_to, l.form_type, l.post_by, l.city,l.update_date ";
 		$sql = " FROM #_leads l WHERE 1=1 AND l.is_deleted=0 AND l.lead_id=0 $adminRole ";
-		$order_by == '' ? $order_by = 'l.post_date' : true;
+		$order_by == '' ? $order_by = 'l.created_date' : true;
 		$order_by2 == '' ? $order_by2 = 'DESC' : true;
 		$sql_count = "SELECT count(*) ".$sql; 
 		$sql .= " order by $order_by $order_by2 ";
